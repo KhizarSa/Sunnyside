3fe27aaa1cc2453b6d7d9e9a3d0442b7e0905d99
KhizarSa <khizarsaeed80@gmail.com>
2022-01-15 19:12:58 +0500

    Initial commit

---
+++ script.js
@@ -0,0 +1,82 @@
+"use strict";
+
+const arr = document.querySelector(".arr-link");
+
+arr.addEventListener("click", function (e) {
+  e.preventDefault();
+  const href = arr.getAttribute("href");
+
+  if (href !== "#" && href.startsWith("#")) {
+    const sectionEl = document.querySelector(href);
+    sectionEl.scrollIntoView({ behavior: "smooth" });
+  }
+});
+
+// Mobile Changes Implementation
+
+// Hamburger Menu:
+const hamburger = document.querySelector(".ham-icon");
+const mobileMenu = document.querySelector(".menu-layout");
+const navList = document.querySelector(".nav-list");
+
+hamburger.addEventListener("click", function () {
+  hamburger.classList.toggle("hide");
+  mobileMenu.classList.toggle("menu-mobile");
+});
+
+// Implementing window resize changes for mobile:
+const mainNav = document.querySelector(".main-nav");
+const galImgs = document.querySelectorAll(".gal-img");
+const featureImgs = document.querySelectorAll(".feature-img");
+
+window.addEventListener(
+  "resize",
+  function (e) {
+    e.preventDefault();
+    if (this.window.innerWidth <= 720) {
+      galImgs.forEach((img) => {
+        img.src = img.src.replace("desktop", "mobile");
+      });
+
+      featureImgs.forEach((img) => {
+        img.src = img.src.replace("desktop", "mobile");
+      });
+
+      document.querySelector(
+        ".last-one"
+      ).style.backgroundImage = `url("../images/mobile/image-graphic-design.jpg")`;
+
+      document.querySelector(
+        ".last-two"
+      ).style.backgroundImage = `url("../images/mobile/image-photography.jpg")`;
+
+      // Implementation for hamburger menu:
+      navList.classList.add("hidden");
+      hamburger.classList.remove("hidden");
+      mobileMenu.classList.remove("hidden");
+    }
+
+    if (this.window.innerWidth > 720) {
+      galImgs.forEach((img) => {
+        img.src = img.src.replace("mobile", "desktop");
+      });
+
+      featureImgs.forEach((img) => {
+        img.src = img.src.replace("mobile", "desktop");
+      });
+
+      document.querySelector(
+        ".last-one"
+      ).style.backgroundImage = `url("../images/desktop/image-graphic-design.jpg")`;
+
+      document.querySelector(
+        ".last-two"
+      ).style.backgroundImage = `url("../images/desktop/image-photography.jpg")`;
+
+      navList.classList.remove("hidden");
+      hamburger.classList.add("hidden");
+      mobileMenu.classList.add("hidden");
+    }
+  },
+  true
+);
---
+++ style-guide.md
@@ -0,0 +1,47 @@
+# Front-end Style Guide
+
+## Layout
+
+The designs were created to the following widths:
+
+- Mobile: 375px
+- Desktop: 1440px
+
+## Colors
+
+### Primary
+
+- Soft red: hsl(7, 99%, 70%) OR #fe7867
+- Yellow: hsl(51, 100%, 49%) OR #fad400
+- Dark desaturated cyan (graphic design text): hsl(167, 40%, 24%) OR #25564b (I like this one #2C7566) For footer text(#418F7D)
+- Dark blue (photography text): hsl(198, 62%, 26%) OR #19536b
+- Dark moderate cyan (footer): hsl(168, 34%, 41%) OR #458c7e (I like this one #96DACC)
+
+### Neutral
+
+- Very dark desaturated blue: hsl(212, 27%, 19%) OR #23303e
+- Very dark grayish blue: hsl(213, 9%, 39%) OR #5a636c
+- Dark grayish blue: hsl(232, 10%, 55%) OR #818498
+- Grayish blue: hsl(210, 4%, 67%) OR #a7abae
+- White: hsl(0, 0%, 100%) OR #ffffff
+
+## Typography
+
+### Body Copy
+
+- Font size: 18px
+
+### Font
+
+- Family: [Barlow](https://fonts.google.com/specimen/Barlow)
+- Weights: 600
+- Family: [Fraunces](https://fonts.google.com/specimen/Fraunces)
+- Weights: 700, 900
+
+## Icons
+
+We provide the required social icons. But, if you prefer, you can use a font icon library. Some suggestions can be found below:
+
+- [Font Awesome](https://fontawesome.com)
+- [IcoMoon](https://icomoon.io)
+- [Ionicons](https://ionicons.com)
